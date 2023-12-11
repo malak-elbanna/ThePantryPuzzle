ffbebcc9a6800ff97e939300d7ec5a181d4120e4
Salma <121863224+SalmaAzeem@users.noreply.github.com>
2023-12-11 13:40:48 +0200

    deleted csv folder

---
+++ Classes/Ingredients.py
@@ -0,0 +1,6 @@
+class Ingredients:
+    def __init__(self, Ingredient_Name, Ingredient_Category):
+        self.Ingredient_Name = Ingredient_Name
+        self.Ingredient_Category = Ingredient_Category
+    def Display_Nutrients(): #access the csv file of the ingredinets 
+        pass 
